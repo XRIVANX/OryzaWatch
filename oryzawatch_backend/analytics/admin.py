6b5c42e0f0b564364e3821df28cee090d69382e1
XRIVANX <i.requerme.142487.tc@umindanao.edu.ph>
2026-08-29 22:04:03 +0800

    feat: register ForecastPrediction model in admin with display and filter options

--- oryzawatch_backend/analytics/admin.py
+++ oryzawatch_backend/analytics/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import DiseaseHotspot
+from .models import DiseaseHotspot, ForecastPrediction
 
 @admin.register(DiseaseHotspot)
 class DiseaseHotspotAdmin(admin.ModelAdmin):
     list_display = ['id', 'scan', 'status', 'temperature', 'wind_cardinal', 'updated_at']
     list_filter = ['status', 'wind_cardinal']
+
+
+@admin.register(ForecastPrediction)
+class ForecastPredictionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'predicted_disease', 'predicted_at', 'verified_disease', 'verified_at']
+    list_filter = ['predicted_disease', 'verified_disease']
 
 # Register your models here.
